639785d83bc8503c5b94c4f2f7c3d149794a18d2
SebaVG <sebastianvelasquezgonzales@gmail.com>
2024-01-28 23:16:17 -0500

    SalaItem

--- src/pages/SalaItemPage.jsx
+++ src/pages/SalaItemPage.jsx
@@ -1,10 +1,28 @@
-import { Container, Box, Grid, Typography, Link, Card, CardMedia, CardContent } from "@mui/material"
+import { Container, Box, Grid, Typography, Link, Card, CardMedia, CardContent, CardActions, Button } from "@mui/material"
 import DrawerBar from "../components/DrawerBar"
 import LocationOnIcon from '@mui/icons-material/LocationOn';
+import Avatar from '@mui/material/Avatar';
+import Stack from '@mui/material/Stack';
 import { CardHeader } from "react-bootstrap";
 
+
 const SalaItemPage = () => {
-  return <Box>
+    
+    const buttonStyle = {
+        width: '80px',
+        height: '28px',
+        padding: '4px 24px',
+        borderRadius: '4px',
+        border: '1px dashed #9747FF',
+        background: 'rgba(151, 71, 255, 0.04)',
+        color: '#9747FF',
+    };
+    
+    const cardStyle = {
+        marginTop: '24px',
+    };
+
+return <Box>
     <DrawerBar />
     <Grid
       container
@@ -29,7 +47,7 @@ const SalaItemPage = () => {
             </Container>
         <Grid container spacing={2}>
         <Grid item xs = {8}>
-            <Card variant="outlined">
+            <Card>
                 <CardMedia
                     component="img"
                     image="https://placehold.co/600x400"
@@ -37,14 +55,14 @@ const SalaItemPage = () => {
             </Card>
         </Grid> 
         <Grid item xs = {4}>
-            <Card variant="outlined">
+            <Card>
                 <CardHeader>
                     <Typography variant="h5" sx={{ padding: 2 }}>
                         Historia
                     </Typography>
                 </CardHeader>
                 <CardContent>
-                    <Typography variant="body1">
+                    <Typography variant="body1" color="text.secondary">
                     La Facultad de Comunicación tiene tres estudios, los cuales son sets profesionales que se utilizan para televisión, streaming y diversas realizaciones audiovisuales. Cada uno de ellos está equipado con tres cámaras de video digitales full HD, switcher digital, pantallas de monitoreo tanto en Control y Estudio, dos tituladores y grabadoras/reproductoras de video. El sonido comprende: consola de audio, micrófonos de mano, de pecho, boom y de vincha. El sistema de iluminación trabaja con control de iluminación, luces alógenas de varios tipos: Fresnel, Scoop, Broad, Cañón, además del rack de control técnico con instrumentos de medición.
                     </Typography>
                 </CardContent>
@@ -58,6 +76,81 @@ const SalaItemPage = () => {
                 </Typography>
             </Grid>
         </Grid>
+        <Container>
+        <Grid>
+            <Grid item xs = {8}>
+            <Card elevation={0} style={cardStyle}>
+                <CardHeader>
+                    <Stack direction="row" spacing={2} sx={{ml: 2, mt: 2}}>
+                        <><Avatar variant="rounded" sx={{background: '#BDBDBD'}}>BS</Avatar></>
+                        <><Typography variant='h6' color="text.primary">Beekeper Sentencia de Muerte</Typography></>
+                    </Stack>
+                </CardHeader>
+                <CardContent>
+                    <Typography variant="body1" color="text.secondary">
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                    </Typography>
+                </CardContent>   
+                <CardActions>
+                    <Button 
+                    variant="outlined"
+                    style={buttonStyle}
+                >15:00</Button>
+                    <Button 
+                    variant="outlined"
+                    style={buttonStyle}
+                    >17:00</Button>
+                </CardActions>
+            </Card>
+            <Card elevation={0} style={cardStyle}>
+                <CardHeader>
+                    <Stack direction="row" spacing={2} sx={{ml: 2, mt: 2}}>
+                        <><Avatar variant="rounded" sx={{background: '#BDBDBD'}}>NG</Avatar></>
+                        <><Typography variant='h6' color="text.primary">El Niño y la Garza</Typography></>
+                    </Stack>
+                </CardHeader>
+                <CardContent>
+                    <Typography variant="body1" color="text.secondary">
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                    </Typography>
+                </CardContent>   
+                <CardActions>
+                    <Button 
+                    variant="outlined"
+                    style={buttonStyle}
+                    >16:00</Button>
+                    <Button 
+                    variant="outlined"
+                    style={buttonStyle}
+                    >18:00</Button>
+                </CardActions>
+            </Card>
+            <Card elevation={0} style={cardStyle}>
+                <CardHeader>
+                    <Stack direction="row" spacing={2} sx={{ml: 2, mt: 2}}>
+                        <><Avatar variant="rounded" sx={{background: '#BDBDBD'}}>JC</Avatar></>
+                        <><Typography variant='h6' color="text.primary">Jack en la Caja Maldita 3</Typography></>
+                    </Stack>
+                </CardHeader>
+                <CardContent>
+                    <Typography variant="body1" color="text.secondary">
+                    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
+                    </Typography>
+                </CardContent>   
+                <CardActions>
+                    <Button 
+                    variant="outlined"
+                    style={buttonStyle}
+                    >20:00</Button>
+                    <Button 
+                    variant="outlined"
+                    style={buttonStyle}
+                    >22:00</Button>
+                </CardActions>
+            </Card>
+            </Grid>
+        </Grid>
+        </Container>
       </Container> 
     </Grid>
   </Box>
