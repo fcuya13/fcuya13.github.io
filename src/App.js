6bc729b16e60605d777f74ac148882c02c146ca5
fcuya13 <francocuya13@gmail.com>
2024-01-30 13:33:08 -0500

    JSON + App small change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import RegisterPage from "./pages/RegisterPage";
@@ -12,7 +12,11 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path="/" Component={HomePage}/>
+        <Route
+            path="*"
+            element={<Navigate to="/login" replace={true} />}
+        />
+        <Route path="/home" Component={HomePage}/>
         <Route path="/login" Component={LoginPage}/>
         <Route path="/registro" Component={RegisterPage}/>
         <Route path="/peliculas" Component={MoviesPage}/>
