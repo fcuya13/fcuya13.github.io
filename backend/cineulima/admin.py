0ed7461045aea2b1d2d4ac47abd2b4c0fb89d61c
fcuya13 <francocuya13@gmail.com>
2024-02-17 16:52:34 -0500

    + recomendacionpelicula

--- backend/cineulima/admin.py
+++ backend/cineulima/admin.py
@@ -12,7 +12,7 @@ class PeliculaAdmin(admin.ModelAdmin):
     list_display = ["titulo", "path"]
 
 
-class PeliculaActor(admin.ModelAdmin):
+class PeliculaActorAdmin(admin.ModelAdmin):
     list_display = ["pelicula_id", "actor"]
 
 
@@ -26,7 +26,7 @@ class SalaAdmin(admin.ModelAdmin):
 
 admin.site.register(Usuario, UsuarioAdmin)
 admin.site.register(Pelicula, PeliculaAdmin)
-admin.site.register(PeliculaActor, UsuarioAdmin)
-admin.site.register(PeliculaGenero, UsuarioAdmin)
+admin.site.register(PeliculaActor, PeliculaActorAdmin)
+admin.site.register(PeliculaGenero, PeliculaGeneroAdmin)
 admin.site.register(Sala, SalaAdmin)
 admin.site.register(RecomendacionPelicula)
